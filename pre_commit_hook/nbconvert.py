ce77ad0de5d10daec2a011fb11cf3800ed45df9e
Vladimir Gurevich <imvladikon@gmail.com>
2020-05-18 19:28:00 +0300

    added precommit hook converting notebooks

---
+++ pre_commit_hook/nbconvert.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+import subprocess
+
+def main(argv=None):
+    return_value = 0
+    filename = sys.argv[1]
+    dir_py = os.path.dirname(filename)
+    # subprocess.run([f'mkdir {dir_py}'], shell=True)
+    subprocess.run([f'git add {dir_py}'], shell=True)
+    fpy = os.path.join(dir_py,  f"{os.path.splitext(os.path.basename(filename))[0]}.py")
+    subprocess.run([f'jupyter nbconvert --to script {filename} --stdout > {fpy}'], shell=True)
+    subprocess.run([f'git add {fpy}'], shell=True)
+    dir_html = os.path.dirname(filename)
+    subprocess.run([f'mkdir {dir_html}'], shell=True)
+    subprocess.run([f'git add {dir_html}'], shell=True)
+    fhtml = os.path.join(dir_html,  f"{os.path.splitext(os.path.basename(filename))[0]}.html")
+    subprocess.run([f'jupyter nbconvert --to html {filename} --stdout > {fhtml}'], shell=True)
+    subprocess.run([f'git add {fhtml}'], shell=True)
+    return return_value
+
+if __name__ == '__main__':
+    exit(main())
