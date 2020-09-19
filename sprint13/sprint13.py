2c6e96e5adc5af2ba76e9e2adf435ac7f7b57ea9
Vladimir Gurevich <imvladikon@gmail.com>
2020-09-19 23:26:48 +0300

    started sprint13

---
+++ sprint13/sprint13.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Project description
+# 
+# Sweet Lift Taxi company has collected historical data on taxi orders at airports. To attract more drivers during peak hours, we need to predict the amount of taxi orders for the next hour. Build a model for such a prediction.
+# 
+# The RMSE metric on the test set should not be more than 48.
+# 
+# ### Project instructions
+# 
+# 1. Download the data and resample it by one hour.
+# 2. Analyze the data.
+# 3. Train different models with different hyperparameters. The test sample should be 10% of the initial dataset. 
+# 4. Test the data using the test sample and provide a conclusion.
+# 
+# ### Data description
+# 
+# The data is stored in file `taxi.csv`. The number of orders is in the '*num_orders*' column.
+
+# # 1. Preparation
+
+# In[ ]:
+
+
+
+
+
+# # 2. Analysis
+
+# In[ ]:
+
+
+
+
+
+# # 3. Training
+
+# In[ ]:
+
+
+
+
+
+# # 4. Testing
+
+# In[ ]:
+
+
+
+
+
+# # Review checklist
+
+# - [x]  Jupyter Notebook is open
+# - [ ]  The code is error-free
+# - [ ]  The cells with the code have been arranged in order of execution
+# - [ ]  The data has been downloaded and prepared
+# - [ ]  The data has been analyzed
+# - [ ]  The model has been trained and hyperparameters have been selected
+# - [ ]  The models have been evaluated. Conclusion has been provided
+# - [ ] *RMSE* for the test set is not more than 48
