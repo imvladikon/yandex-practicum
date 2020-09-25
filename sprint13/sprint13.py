e33e6061e8dea14a864dbfa084c170f59fba89c2
Vladimir Gurevich <imvladikon@gmail.com>
2020-09-25 19:41:10 +0300

    finished sprint13

--- sprint13/sprint13.py
+++ sprint13/sprint13.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# Hello, my name is Artem. I'm going to review your project!
+# 
+# You can find my comments in <font color='green'>green</font>, <font color='blue'>blue</font> or <font color='red'>red</font> boxes like this:
+# 
+# <div class="alert alert-block alert-success">
+# <b>Success:</b> if everything is done succesfully
+# </div>
+# 
+# <div class="alert alert-block alert-info">
+# <b>Improve: </b> "Improve" comments mean that there are tiny corrections that could help you to make your project better.
+# </div>
+# 
+# <div class="alert alert-block alert-danger">
+# <b>Needs fixing:</b> if the block requires some corrections. Work can't be accepted with the red comments.
+# </div>
+# 
+# ### <font color='orange'>General feedback</font>
+# * Your project looks pretty good! 
+# * I'm impressed with code formatting in your project. 
+# * There are a couple of things that need to be done before your project is complete, but they're pretty straightforward.
+# * You can also make your project even better if you work on the "improve" comments.
+# * You're almost there! Good luck!
+
+# >Thank you!
+
 # ## Project description
 # 
 # Sweet Lift Taxi company has collected historical data on taxi orders at airports. To attract more drivers during peak hours, we need to predict the amount of taxi orders for the next hour. Build a model for such a prediction.
@@ -18,13 +43,13 @@
 # 
 # The data is stored in file `taxi.csv`. The number of orders is in the '*num_orders*' column.
 
-# In[1]:
+# In[78]:
 
 
 get_ipython().system('pip install -qq catboost')
 
 
-# In[2]:
+# In[79]:
 
 
 import numpy as np
@@ -68,7 +93,11 @@ logger = logging.getLogger()
 logger.setLevel(logging.CRITICAL)
 
 
-# In[3]:
+# <div class="alert alert-block alert-success">
+# <b>Success:</b> Thank you for collecting all imports in the first cell!
+# </div>
+
+# In[80]:
 
 
 def adfuller_stats(values):
@@ -163,49 +192,53 @@ def plotCoefficients(model):
 
 # # 1. Preparation
 
-# In[4]:
+# In[81]:
 
 
 pd.read_csv("https://code.s3.yandex.net/datasets/taxi.csv").head()
 
 
-# In[5]:
+# In[82]:
 
 
 df = pd.read_csv("https://code.s3.yandex.net/datasets/taxi.csv", index_col=['datetime'], parse_dates=['datetime'])
 
 
-# In[6]:
+# In[83]:
 
 
 df = df.resample('1h').sum().sort_index()
 
 
-# In[7]:
+# In[84]:
 
 
 df.info()
 
 
-# In[8]:
+# In[85]:
 
 
 df.head()
 
 
-# In[9]:
+# In[86]:
 
 
 target = "num_orders"
 
 
+# <div class="alert alert-block alert-success">
+# <b>Success:</b> Data loading and initial analysis were done well. Resempling was done correctly!
+# </div>
+
 # # 2. Analysis
 
-# In[10]:
+# In[87]:
 
 
 def prepare_data(df, target="num_orders", window_size=6, n_lag=25):
-  data = df[:]
+  data = df.copy()
   data['rolling_mean'] = data[target].shift().rolling(window_size).mean()
   for lag in range(1, n_lag + 1):
     data["lag_{}".format(lag)] = data[target].shift(lag)#, fill_value=0)
@@ -219,13 +252,22 @@ def prepare_data(df, target="num_orders", window_size=6, n_lag=25):
   return data
 
 
-# In[11]:
+# <div class="alert alert-block alert-info">
+# <b>Improve: </b> 
+#     
+# You could use `df.copy()` for copying the dataframe.
+#     
+# </div>
+
+# >+
+
+# In[88]:
 
 
 data = prepare_data(df)
 
 
-# In[38]:
+# In[89]:
 
 
 plt.figure(figsize=(16, 5))
@@ -238,7 +280,7 @@ plt.grid(True);
 
 #  Augmented Dickey-Fuller test determine if the series is stationary, let's test it and check p-value
 
-# In[12]:
+# In[90]:
 
 
 with plt.style.context('bmh'):  
@@ -252,7 +294,7 @@ with plt.style.context('bmh'):
     plt.grid(True)
 
 
-# In[13]:
+# In[91]:
 
 
 adfuller_stats(data["num_orders"].values)
@@ -261,9 +303,13 @@ adfuller_stats(data["num_orders"].values)
 # let's assuming significance α=0.01, therefore we could not reject the null hypothesis, and classify this as stationary
 # 
 
+# <div class="alert alert-block alert-success">
+# <b>Success:</b> Glad to see that you've made this check!
+# </div>
+
 # let's plot rolling average (and std)
 
-# In[14]:
+# In[92]:
 
 
 rolling_means_plt(data["num_orders"], window=30)
@@ -271,7 +317,11 @@ rolling_means_plt(data["num_orders"], window=30)
 
 # let's try do stationary model
 
-# In[15]:
+# <div class="alert alert-block alert-success">
+# <b>Success:</b> Great that original time series and rolling mean were visualized!
+# </div>
+
+# In[93]:
 
 
 tsplot_plt(data["num_orders"]) 
@@ -279,7 +329,7 @@ tsplot_plt(data["num_orders"])
 
 # let's plot autocorellation graphs
 
-# In[16]:
+# In[94]:
 
 
 plot_acf(data["num_orders"].rolling(window=3).mean().dropna())
@@ -290,7 +340,7 @@ plt.show()
 
 # as we could see , we have seasonality, which we have to deal with prior to modeling. let's take the a simple subtraction of the series from itself with a lag that equals the seasonal period
 
-# In[17]:
+# In[95]:
 
 
 ads_diff = data["num_orders"] - data["num_orders"].shift(23)
@@ -304,7 +354,7 @@ tsplot_plt(ads_diff[23:], lags=60)
 
 # for a week
 
-# In[18]:
+# In[96]:
 
 
 seasonal_decompose(data['2018-05-01':'2018-05-31'].resample('1D').sum().loc[:, target]).seasonal.plot()
@@ -312,48 +362,82 @@ seasonal_decompose(data['2018-05-01':'2018-05-31'].resample('1D').sum().loc[:, t
 
 # for a day
 
-# In[19]:
+# In[97]:
 
 
 seasonal_decompose(data['2018-05-01':'2018-05-07'].loc[:, 'num_orders']).seasonal.plot(ax=plt.gca())
 
 
+# <div class="alert alert-block alert-success">
+# <b>Success:</b> Great that you've made different visualization of seasonality.
+# </div>
+
+# <div class="alert alert-block alert-info">
+# <b>Improve: </b> Please, don't forget about trend.
+# </div>
+
+# >+, forgot;)
+
+# In[98]:
+
+
+seasonal_decompose(data['2018-05-01':'2018-05-31'].resample('1D').sum().loc[:, target]).trend.plot()
+
+
+# In[99]:
+
+
+seasonal_decompose(data['2018-05-01':'2018-05-07'].loc[:, 'num_orders']).trend.plot(ax=plt.gca())
+
+
+# In[100]:
+
+
+result = seasonal_decompose(data['2018-05-01':'2018-05-14'].loc[:, 'num_orders'], model='multiplicative')
+result.plot()
+plt.show()
+
+
+# In[101]:
+
+
+result = seasonal_decompose(data['2018-05-01':'2018-05-14'].loc[:, 'num_orders'], model='additive')
+result.plot()
+plt.show()
+
+
+# as we could see,  the trend and seasonality extracted from the series does seem reasonable. according to residuals, we could see periods of high variability in the early and later periods(days) of the series
+
 # # 3. Training
 
 # # 4. Testing
 
-# In[20]:
+# In[102]:
 
 
 target = "num_orders"
 features = set(data.columns)-set([target])
 
 
-# In[21]:
+# <div class="alert alert-block alert-info">
+# <b>Improve: </b> You could use train_test_split with 'shuffle=False' instead.
+# </div>
 
+# >fixed it
 
-def timeseries_train_test_split(X, y, test_size):
-    test_index = int(len(X)*(1-test_size))
-    X_train = X.iloc[:test_index]
-    y_train = y.iloc[:test_index]
-    X_test = X.iloc[test_index:]
-    y_test = y.iloc[test_index:]
-    return X_train, X_test, y_train, y_test
-
-
-# In[22]:
+# In[103]:
 
 
 X, y = data[features], data[target]
 
 
-# In[23]:
+# In[104]:
 
 
-X_train, X_test, y_train, y_test = timeseries_train_test_split(X, y, 0.1)#train_test_split(data[features].values, data[target].values, shuffle=False, test_size=0.1)
+X_train, X_test, y_train, y_test = train_test_split(data[features], data[target], shuffle=False, test_size=0.1)
 
 
-# In[24]:
+# In[105]:
 
 
 print(X_train.index.min(), X_train.index.max())
@@ -362,7 +446,7 @@ print(X_test.index.min(), X_test.index.max())
 
 # ### LinearRegression
 
-# In[25]:
+# In[106]:
 
 
 rows = []
@@ -375,7 +459,7 @@ stats_df = pd.DataFrame(rows)
 stats_df
 
 
-# In[26]:
+# In[107]:
 
 
 lr = LinearRegression()
@@ -384,22 +468,34 @@ plotCoefficients(lr)
 plotModelResults(lr, X_train, X_test, y_train, y_test)
 
 
-# In[27]:
+# <div class="alert alert-block alert-info">
+# <b>Improve: </b> This coefficients are useless because scale of features is different.
+# </div>
+
+# >agree!
+
+# In[108]:
 
 
 stats_df.iloc[stats_df["RMSE"].argmin()]
 
 
-# In[28]:
+# In[109]:
 
 
 best_shifted = int(stats_df.iloc[stats_df["RMSE"].argmin()]["shifted"])
 best_shifted
 
 
+# <div class="alert alert-block alert-info">
+# <b>Improve: </b> What for you were adding features one by one? 
+# </div>
+
+# idea was, find some optimal result adding features by features and that's why I also saw best coeff. do the same strategy in the usual way, but in the time series case feel that's it's a bit tricky. if I understood correctly, the righteous way is doing ARIMA based model (for classical ML)
+
 # ### RandomForestRegressor
 
-# In[29]:
+# In[110]:
 
 
 model = RandomForestRegressor().fit(X_train, y_train)
@@ -408,7 +504,7 @@ y_pred = model.predict(X_test)
 print(rmse_error(y_pred, y_test))
 
 
-# In[30]:
+# In[111]:
 
 
 param_grid ={'n_estimators': range(50, 60, 5),
@@ -419,21 +515,33 @@ param_grid ={'n_estimators': range(50, 60, 5),
 
 # lets use mae insted of rmse for avoiding negative results as score
 
-# In[31]:
+# In[112]:
 
 
 rmse_scorer = make_scorer(rmse_error, greater_is_better=False)
 
 
-# In[32]:
+# In[113]:
 
 
 model = RandomForestRegressor(n_estimators=100)
-clf = GridSearchCV(model, scoring=rmse_scorer, param_grid=param_grid, n_jobs=4, cv=5).fit(X_train, y_train)
+tscv = TimeSeriesSplit(n_splits=5)
+clf = GridSearchCV(model, scoring=rmse_scorer, param_grid=param_grid, n_jobs=4, cv=tscv).fit(X_train, y_train)
 best_params = clf.best_params_
+print(clf.best_score_)
 
 
-# In[33]:
+# <div class="alert alert-block alert-info">
+# <b>Improve: </b> Please, print best validation score.
+# </div>
+
+# <div class="alert alert-block alert-danger">
+# <b>Needs fixing:</b> You should use TimeSeriesSplit() from sklearn as 'cv' parameter because default cross validation could not be used with time series data. It shuffles folds and train data can be from future and test from past.
+# </div>
+
+# completely agree, probably missed it occasionally, in the function plotModelResults, (in plot_intervals case) used TimeSeriesSplit, in this case made a mistake;)
+
+# In[114]:
 
 
 model = RandomForestRegressor(**best_params).fit(X_train, y_train)
@@ -442,14 +550,18 @@ y_pred = model.predict(X_test)
 print(rmse_error(y_pred, y_test))
 
 
-# In[34]:
+# In[115]:
 
 
 model = RandomForestRegressor(**best_params).fit(X_train, y_train)
 plotModelResults(model, X_train, X_test, y_train, y_test)
 
 
-# In[35]:
+# <div class="alert alert-block alert-success">
+# <b>Success:</b> Great that you've visualized the result.
+# </div>
+
+# In[116]:
 
 
 best_params
@@ -458,6 +570,140 @@ best_params
 # # Summary
 
 # we found that our model is non stationary, we built linear model and tree based model and found best result for tree based model and with rmse less than 41 which is enough. also we checked daily changes, week changes and found drift around 22-23 days
+
+# ## Additional model: LSTM
+
+# I was curious about checking some simple LSTM model for the case
+
+# In[197]:
+
+
+import torch
+import torch.nn as nn
+
+
+# In[198]:
+
+
+use_gpu = lambda x=True: torch.set_default_tensor_type(torch.cuda.FloatTensor 
+                                             if torch.cuda.is_available() and x 
+                                             else torch.FloatTensor)
+
+
+# In[199]:
+
+
+use_gpu()
+
+
+# In[200]:
+
+
+device = torch.device("cuda")
+
+
+# In[201]:
+
+
+train_data = torch.FloatTensor(X_train.values).to(device)
+test_data = torch.FloatTensor(X_test.values).to(device)
+
+
+# In[207]:
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size=1, hidden_layer_size=150, output_size=1):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
+
+        self.lstm = nn.LSTM(input_size, hidden_layer_size, num_layers=1, dropout=0.3)
+
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+
+        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
+                            torch.zeros(1,1,self.hidden_layer_size))
+        
+     
+    def to(self, device):
+        super(LSTM, self).to(device)
+        self.lstm = self.lstm.to(device)
+        self.linear = self.linear.to(device)
+        self.hidden_cell = tuple(map(lambda t: t.to(device), self.hidden_cell))
+        self.device = device
+        return self
+
+    def reset_hidden_state(self):
+        if self.device is not None:
+            self.hidden_cell = (torch.zeros(1, 1, self.hidden_layer_size).to(self.device),
+                            torch.zeros(1, 1, self.hidden_layer_size).to(self.device))
+        else:
+            self.hidden_cell = (torch.zeros(1, 1, self.hidden_layer_size),
+                            torch.zeros(1, 1, self.hidden_layer_size))
+
+    def forward(self, input_seq):
+        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
+        predictions = self.linear(lstm_out.view(len(input_seq), -1))
+        return predictions[-1]
+
+
+# In[203]:
+
+
+class RMSELoss(nn.Module):
+    def __init__(self, eps=1e-6):
+        super().__init__()
+        self.mse = nn.MSELoss()
+        self.eps = eps
+        
+    def forward(self,yhat,y):
+        loss = torch.sqrt(self.mse(yhat,y) + self.eps)
+        return loss
+
+
+# In[208]:
+
+
+model = LSTM().to(device)
+loss_function = RMSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+# In[209]:
+
+
+epochs = 150
+model.train()
+for i in range(epochs):
+    for seq, labels in zip(train_data, y_train):
+        optimizer.zero_grad()
+        model.reset_hidden_state()
+        y_pred = model(seq)
+        label = torch.FloatTensor([labels]).to(device)
+        single_loss = loss_function(y_pred, label)
+        single_loss.backward()
+        optimizer.step()
+
+    if i%25 == 0:
+        print(f'epoch: {i:3} loss: {single_loss.item():10.8f}')
+
+print(f'epoch: {i:3} loss: {single_loss.item():10.10f}')
+
+
+# In[210]:
+
+
+model.eval()
+y_pred = []
+for i in range(len(test_inout_seq)):
+  with torch.no_grad():
+    model.reset_hidden_state()
+    y_pred.append(model(test_inout_seq[i]).item())
+y_pred = np.array(y_pred)
+print(rmse_error(y_pred, y_test))
+
+
+# as we see, some simple LSTM model gave us really nice result,  less than 48
 
 # # References:
 # 
